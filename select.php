25165226dcd417afc6cb9804279cfb22b903d287
justinjaywang <justinjaywang@gmail.com>
2013-03-05 07:31:30 -0800

    autofocus

--- select.php
+++ select.php
@@ -1,6 +1,6 @@
 <?php
 $numParagraphs = intval($_POST["p"]);
-echo '<select name="p" onchange="this.form.submit()">';
+echo '<select autofocus name="p" onchange="this.form.submit()">';
 echo '<option value="">¶</option>';
 for ($i=1; $i<=12; $i++) {
   if ($i==$numParagraphs) {
