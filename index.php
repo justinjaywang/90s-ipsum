44e25c62bdc0f89bc5946c1c25ebf5a32208c042
Justin Wang <justinjaywang@gmail.com>
2015-12-06 10:51:08 -0500

    update color palette

--- index.php
+++ index.php
@@ -3,44 +3,36 @@
 <?php
 include 'words.php'; // $allWords, $ninetiesWords
 include 'generate.php'; // $paragraphs
-$fgColors = array(
-  "mediumvioletred",
-  "firebrick",
-  "darkred",
-  "darkgreen",
-  "darkslateblue",
-  "blue",
-  "mediumblue",
-  "darkblue",
-  "navy",
-  "indigo",
-  "darkmagenta",
-  "darkviolet",
-  "purple"
+$palettes = array (
+  array("Black", "LightSteelBlue"),
+  array("WhiteSmoke", "OrangeRed"),
+  array("LightYellow", "RebeccaPurple"),
+  array("DarkSlateBlue", "PowderBlue"),
+  array("LightCoral", "Yellow"),
+  array("SteelBlue", "Aquamarine"),
+  array("Gainsboro", "Teal"),
+  array("MidnightBlue", "DeepPink"),
+  array("LightCoral", "Black"),
+  array("LightSteelBlue", "Teal"),
+  array("OrangeRed", "LightYellow"),
+  array("Indigo", "White"),
+  array("PowderBlue", "SlateBlue"),
+  array("Yellow", "SlateBlue"),
+  array("Aquamarine", "MidnightBlue"),
+  array("Teal", "LightSteelBlue"),
+  array("DeepPink", "Indigo"),
+  array("Black", "Aquamarine"),
+  array("DarkSlateBlue", "Yellow"),
+  array("MidnightBlue", "LightCoral"),
+  array("Teal", "White"),
+  array("DarkSlateBlue", "Aquamarine"),
+  array("MediumSpringGreen", "Black"),
+  array("Gainsboro", "DeepPink")
 );
-$bgColors = array(
-  "lavenderblush",
-  "mistyrose",
-  "seashell",
-  "linen",
-  "peachpuff",
-  "cornsilk",
-  "ivory",
-  "lemonchiffon",
-  "lightgoldenrodyellow",
-  "mintcream",
-  "azure",
-  "honeydew",
-  "aliceblue",
-  "ghostwhite",
-  "lavender",
-  "thistle",
-  "whitesmoke"
-);
-shuffle($fgColors);
-$fgColor = next($fgColors);
-shuffle($bgColors);
-$bgColor = next($bgColors);
+shuffle($palettes);
+$palette = next($palettes);
+$bgColor = $palette[0];
+$fgColor = $palette[1];
 ?>
 <head>
   <meta charset="utf-8">
@@ -48,6 +40,16 @@ $bgColor = next($bgColors);
   <meta name="description" content="Dope filler text for your design project.">
   <meta name="author" content="Justin Wang, a child of the 90s." />
   <link rel="stylesheet" href="style.php">
+  <style type="text/css">
+    ::-moz-selection {
+      background-color: <?php echo $fgColor; ?>;
+      color: <?php echo $bgColor; ?>;
+    }
+    ::selection {
+      background-color: <?php echo $fgColor; ?>;
+      color: <?php echo $bgColor; ?>;
+    }
+  </style>
 </head>
 <?php
 echo "<body style='color:" . $fgColor . "; background-color:" . $bgColor . ";'>";
